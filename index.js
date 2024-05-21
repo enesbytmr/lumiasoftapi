ec08358ccd92cc91d0e6ce37a29fae8f35786411
enesbytmr <enesbytmr@gmail.com>
2024-05-22 02:41:32 +0300

    update for pdf download

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ app.use(cors()); // CORS ayarları ekleniyor
 // GET İsteğini Karşılayan Endpoint
 app.get('/download-pdf', async (req, res) => {
     try {
-        const response = await axios.get('http://amzn-gift-message.eastus.azurecontainer.io/download-pdf');
+        const response = await axios.get('http://amzn-gift-message.eastus.azurecontainer.io/download-pdf', {
+            responseType: 'arraybuffer'
+        });
 
-        res.status(response.status).json({ message: 'GET request sent successfully', data: response.data });
+        const filePath = path.join(__dirname, 'downloaded.pdf');
+        fs.writeFileSync(filePath, response.data);
+
+        res.download(filePath, 'downloaded.pdf', (err) => {
+            if (err) {
+                console.error('Error sending the file:', err);
+                res.status(500).send('Error sending the file');
+            }
+            // Optionally delete the file after sending it
+            fs.unlinkSync(filePath);
+        });
     } catch (error) {
+        console.error('Error downloading the file:', error);
         res.status(error.response?.status || 500).json({ error: error.message });
     }
 });
+
 
 // POST İsteğini Karşılayan Endpoint
 app.post('/create-pdf', async (req, res) => {
