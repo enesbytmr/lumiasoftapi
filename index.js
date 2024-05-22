84113a9575c5988c9b027be9c2d6ed3738cb4915
enesbytmr <enesbytmr@gmail.com>
2024-05-22 10:59:27 +0300

    update for deleted post method

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const app = express();
 const fs = require('fs');
 const path = require('path'); // path modülünü import ediyoruz
-const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -25,6 +25,26 @@ app.get('/download-pdf', async (req, res) => {
     }
 });
 
+// POST İsteğini Karşılayan Endpoint
+app.post('/create-pdf', async (req, res) => {
+    try {
+        const { receiver, order_number, gift_message } = req.body;
+
+        const response = await axios.post('http://amzn-gift-message.eastus.azurecontainer.io/create-pdf', {
+            receiver,
+            order_number,
+            gift_message
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: error.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
